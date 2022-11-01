a71f90e7caef2c270a96be2cbf206f37d8360829
niklassendelbach <niklas@sendelbach.se>
2022-11-01 10:17:43 +0100

    Add project files.

---
+++ Lektion 221101 övning encaps/Customer.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lektion_221101_övning_encaps
+{
+    class Customer
+    {
+        private int id;
+        private string firstName;
+        private string lastName;
+        private string email;
+
+        public int Id 
+        { 
+            get
+            {
+                return id;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    Console.WriteLine("Värdet får inte vara mindre än 0");
+                }
+                else
+                {
+                    id = value;
+                }
+            }
+        }
+        public string FirstName 
+        { 
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                if (value == string.Empty)
+                {
+                    throw new Exception("First name can't be empty");
+                }
+                else if (value == null)
+                {
+                    FirstName = "No first name";
+                }
+                else
+                {
+                    firstName = value;
+                }
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                if (value == string.Empty)
+                {
+                    throw new Exception("Last name can't be empty");
+                }
+                else if (value == null)
+                {
+                    LastName = "No last name";
+                }
+                else
+                {
+                    lastName = value;
+                }
+            }
+        }
+        public string Email 
+        { 
+            get
+            {
+                return email;
+            }
+            set
+            {
+                if (value == string.Empty)
+                {
+                    throw new Exception("Email can't be empty");
+                }
+                else if (value == null)
+                {
+                    Email = "Mail is empty";
+                }
+                else
+                {
+                    email = value;
+                }
+            }
+        }
+        public void printInfo()
+        {
+            Console.WriteLine($"ID = {id} \nFirst name = {firstName} \nLast name = {lastName} \nEmail = {email}");
+        }
+    }
+}
---
+++ Lektion 221101 övning encaps/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Lektion_221101_övning_encaps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer C1 = new Customer();
+            C1.Id = 100;
+            C1.FirstName = "Niklas";
+            C1.LastName = "Sendelbach";
+            C1.Email = "niklas@sendelbach.se";
+            C1.printInfo();
+            Console.WriteLine(new string('~', 30));
+            Customer C2 = new Customer();
+            C2.Id = 110;
+            C2.FirstName = "Belle";
+            C2.LastName = "Sendelbach";
+            C2.Email = "belle@sendelbach.se";
+            C2.printInfo();
+            Console.WriteLine(new string('~', 30));
+            Customer C3 = new Customer();
+            C3.Id = -1;
+            C3.FirstName = null;
+            C3.LastName = null;
+            C3.Email = null;
+            C3.printInfo();
+
+
+
+        }
+    }
+}
